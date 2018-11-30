dc37dc90592a32240fc1ac03be1dcab7df255688
balakumarmb <33512797+balakumarmb@users.noreply.github.com>
2018-11-30 11:27:33 +0530

    Update TestController.java

--- demo/src/main/java/com/example/demo/TestController.java
+++ demo/src/main/java/com/example/demo/TestController.java
@@ -7,18 +7,26 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 
-@RequestMapping("/api/**")
+
 @RestController
 public class TestController {
-	@Value("${first_name}")
+	@Value("${first.name}")
 	private String firstName;
 	
-	@Value("${last_name}")
+	@Value("${last.name}")
 	private String lastName;
 	
 	
-    @RequestMapping(method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
+    @RequestMapping(value ="/api/**",method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
     public Widget index() {
         return new Widget(firstName+lastName, 10, 7);
     }
+    
+    
+    @RequestMapping(value ="/",method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
+    public Widget index1() {
+        return new Widget("Mugilan", 100, 70);
+    }
+    
+    
 }
